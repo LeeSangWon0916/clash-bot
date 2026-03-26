30903db82909f1f41000ff81b2cf5d80eb62fea2
이상원 <yap2352@naver.com>
2026-03-27 08:50:29 +0900

    Add specific time

--- bot.py
+++ bot.py
@@ -106,7 +106,7 @@ async def daily_task(channel_a, channel_b):
     KST = timezone(timedelta(hours=9))
     while True:
         now_kst = datetime.now(KST)
-        target_time = now_kst.replace(hour=23, minute=57, second=0, microsecond=0)
+        target_time = now_kst.replace(hour=8, minute=55, second=0, microsecond=0)
 
         if now_kst >= target_time:
             target_time += timedelta(days=1)
