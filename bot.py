96b78b60ef975094bae4d1fdd2b04edbbe0c6f32
이상원 <yap2352@naver.com>
2026-05-19 12:46:31 +0900

    update

--- bot.py
+++ bot.py
@@ -303,7 +303,7 @@ async def daily_task(channel_a, channel_b):
 
     while True:
         now_kst = datetime.now(KST)
-        target_time = now_kst.replace(hour=12, minute=37, second=0, microsecond=0)
+        target_time = now_kst.replace(hour=12, minute=48, second=0, microsecond=0)
 
         if now_kst >= target_time:
             target_time += timedelta(days=1)
